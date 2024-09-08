a0e192c92e73f52ea7c21b3283d82158a1fffabc
uzairlatif90 <33296015+uzairlatif90@users.noreply.github.com>
2024-09-08 12:26:38 +0200

    Update IceRayTracing.cc
    correct bug for rounding off in double exponent profile case

--- namespace/wROOT/RadarPlot/IceRayTracing.cc
+++ namespace/wROOT/RadarPlot/IceRayTracing.cc
@@ -1798,7 +1798,16 @@ double *IceRayTracing::IceRayTracing(double x0, double z0, double x1, double z1)
 
   /* Store the ray paths in text files */
   bool PlotRayPaths=false;
-  
+
+  if(IceRayTracing::TransitionBoundary!=0){
+    if(fabs(round(z0*1.e9)/1.e9)==IceRayTracing::TransitionBoundary){
+      z0=-(IceRayTracing::TransitionBoundary-0.00001);
+    }
+    if(fabs(round(z1*1.e9)/1.e9)==IceRayTracing::TransitionBoundary){
+      z1=-(IceRayTracing::TransitionBoundary-0.00001);
+    }
+  }
+	
   double Txcor[2]={x0,z0};/* Tx positions */
   double Rxcor[2]={x1,z1};/* Rx Positions */
   
